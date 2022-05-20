801224636fdc78f462a89206f4147d6147b1728f
higor-jardim <higor_jardim@hotmail.com>
2022-05-20 18:00:04 -0300

    implemented random level generator

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -8,13 +8,24 @@ public class LevelManager : MonoBehaviour
     
     public List<GameObject> levels;
 
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+    public int piecesNumber = 5;
+    public int piecesStartNumber = 3;
+    public int piecesEndNumber = 1;
+
     public int _index;
 
     private GameObject _currentLevel;
 
+    private List<LevelPieceBase> _spawnedPieces;
+
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -31,12 +42,45 @@ public class LevelManager : MonoBehaviour
         }
         
         _currentLevel = Instantiate(levels[_index], container);
-        //_currentLevel.transform.localPosition = Vector3.zero;
+        _currentLevel.transform.localPosition = Vector3.zero;
     }
 
     private void ResetLevelIndex()
     {
         _index = 0;
     }
+
+    #region
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = levelPieces[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < piecesStartNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+        }
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+        }
+        for (int i = 0; i < piecesEndNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+        }
+    }
+    #endregion
 }
 
